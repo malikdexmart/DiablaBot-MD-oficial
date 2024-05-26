246fb25020142345b238c41f611ce9993a5d05e2
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-05-26 05:16:37 -0300

    Actualizar descargas-playdoc.js

--- plugins/descargas-playdoc.js
+++ plugins/descargas-playdoc.js
@@ -7,7 +7,7 @@ import {youtubedl, youtubedlv2} from '@bochilteam/scraper'
 const handler = async (m, {conn, command, args, text, usedPrefix}) => {
 
 if (!text) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *іᥒgrᥱs ᥱᥣ nombre ძᥱ ᥙᥒ ᥎іძᥱ᥆ ძᥱ ᥡ᥆ᥙ𝗍ᥙᑲᥱ*\n\n[ 💡 ]ᥱȷᥱm⍴ᥣ᥆, !${command} New West - Those Eyes`,  m, fake, )
-
+m.react('🌹')
 
 try {
 
