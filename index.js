dddd5ea97452984e853ceb78acbba2a604396cd6
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-26 20:54:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Bailey\nBot\nMD', {
+say('Diabla\nBot\nMD', {
 font: 'block',
 align: 'center',
 colors: ['BlueBright', 'cyan']
