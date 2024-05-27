50b4e1d66efc741edf99f03c06306c80d9f9f657
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-05-27 06:10:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Diabla\nBot\nMD', {
+say('Bailey\nBot\nMD', {
 font: 'block',
 align: 'center',
 colors: ['BlueBright', 'cyan']
