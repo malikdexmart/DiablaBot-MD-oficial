d4bedce32811c756dc2ee478ea20deb5a98c7ae5
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-05-28 05:31:35 -0300

    Actualizar Bot-Gay3.js

--- plugins/Bot-Gay3.js
+++ plugins/Bot-Gay3.js
@@ -17,8 +17,8 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 > 👀 *Vistas:* ${views}
 > └──────────────`
  await conn.sendButton(m.chat, play, fgig, thumbnail, [
-    ['🍧 MP3', `${usedPrefix}fgmp3 ${url}`],
-    ['🍿 MP4', `${usedPrefix}fgmp4 ${url}`]
+    ['💫 𝗔𝗨𝗗𝗜𝗢', `${usedPrefix}fgmp3 ${url}`],
+    ['✨ 𝗩𝗜𝗗𝗘𝗢', `${usedPrefix}fgmp4 ${url}`]
   ], m, fake)
 }
 handler.help = ['bot']
