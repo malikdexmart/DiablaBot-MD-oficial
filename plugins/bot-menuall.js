5b9f910def70a8209c4db970314c2f5777a5899d
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-05-30 08:13:13 -0300

    Actualizar bot-menuall.js

--- plugins/bot-menuall.js
+++ plugins/bot-menuall.js
@@ -169,7 +169,7 @@ const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://teleg
 await conn.reply(m.chat, '✰【𝐄𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐄𝐥 𝐌𝐞𝐧𝐮】✰ . . .*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '🍁 𝓓𝓘𝓐𝓑𝓛𝓐 - 𝓑𝓞𝓣 - 𝓜𝓓 🌸', body: '👋 Hola ' + name, sourceUrl: global.md, thumbnail: await (await fetch(pp)).buffer() }}})
     
     let pp1 = 'https://telegra.ph/file/33d3e7b5d8d455e03136c.mp4'
-    let pp2 = 'https://telegra.ph/file/fef813ac00aa1cca18d17.mp4'
+    let pp2 = 'https://telegra.ph/file/e7dd02ec206a0edde1b74.mp4'
 
 
     conn.sendMessage(m.chat, { video: { url: [pp1, pp2].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: m }) 
