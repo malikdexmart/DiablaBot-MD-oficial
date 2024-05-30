7c9b95e9d5be88fea05174fe53fa62eae2238074
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-05-30 07:04:34 -0300

    Update main.js

--- main.js
+++ main.js
@@ -110,7 +110,8 @@ const {state, saveState, saveCreds} = await useMultiFileAuthState(global.authFil
 const msgRetryCounterMap = (MessageRetryMap) => { };
 const msgRetryCounterCache = new NodeCache()
 const {version} = await fetchLatestBaileysVersion();
-let phoneNumber = global.botNumberCode();
+let phoneNumber = global.botNumberCode{ };
+
 filesInit().then((_) => Object.keys(global.plugins)).catch(console.error)
 
 global.reload = async (_ev, filename) => {
