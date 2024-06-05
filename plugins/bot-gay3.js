ae2a873a7762ee3685f9668bd808f69f1860f041
Enzo-PL-Oficial <106297033+EnzoVaselevich@users.noreply.github.com>
2024-06-05 08:25:24 -0400

    Actualizar bot-gay3.js

--- plugins/bot-gay3.js
+++ plugins/bot-gay3.js
@@ -6,7 +6,7 @@ let chat = global.db.data.chats[m.chat]
 if (/^Hola|ola$/i.test(m.text) && !chat.isBanned) {
 
 conn.sendPresenceUpdate('composing', m.chat)    
-conn.reply(m.chat, '*🍿 Hola, Como éstas?*\n*Pon .allmenu para ver mi lista de comandos*', m, fake, )}
+conn.reply(m.chat, '*💖 𝗛𝗼𝗹𝗮, 𝗖𝗼𝗺𝗼 𝗲𝘀𝘁𝗮𝘀?*\n*𝗣𝗼𝗻 .allmenu 𝗽𝗮𝗿𝗮 𝘃𝗲𝗿 𝗺𝗶 𝗹𝗶𝘀𝘁𝗮 𝗱𝗲 𝗰𝗼𝗺𝗮𝗻𝗱𝗼𝘀*', m, fake, )}
 
 return !0
 
