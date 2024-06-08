a8acdfe9926d729b47f2418e39cce26620d3bb40
Enzo-PL-Oficial <106297033+EnzoVaselevich@users.noreply.github.com>
2024-06-08 13:09:52 -0300

    Actualizar rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
   
 user.registered = false
-m.reply(`*🪷 𝐔𝐒𝐓𝐄𝐃 𝐘𝐀 𝐍𝐎 𝐄𝐒𝐓𝐀 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐀𝐃𝐎*`)
+m.reply(`*[🪷] ᩭ𝐔𝐒𝐓𝐄𝐃 𝐘𝐀 𝐍𝐎 𝐄𝐒𝐓𝐀 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐀𝐃𝐎ঔৣ᭢*`)
 
 }
 handler.command = ['unreg']
