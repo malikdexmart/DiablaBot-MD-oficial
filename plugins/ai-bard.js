5d2865681907f021fbff6e18d69182a0435fb3eb
Enzo-PL-Oficial <106297033+EnzoVaselevich@users.noreply.github.com>
2024-06-22 02:56:03 -0300

    Actualizar ai-bard.js

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -1,26 +1,28 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
-var handler = async (m, { text,  usedPrefix, command }) => {
+var handler = async (m, { text, usedPrefix, command, conn }) => {
+  if (!text) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *іᥒgrᥱsᥱ ᥙᥒᥲ ⍴ᥱ𝗍іᥴі᥆́ᥒ*\n\n[ ❕ ]Ejemplo, ${usedPrefix}${command} BaileyBot-MD?`, m)
 
-if (!text) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *іᥒgrᥱsᥱ ᥙᥒᥲ ⍴ᥱ𝗍іᥴі᥆́ᥒ*\n\n[ ❕ ]Ejemplo, !bard ᥴ᥆ᥒ᥆ᥴᥱs BaileyBot-MD?`, m, fake, )
+  try {
+    conn.sendPresenceUpdate('composing', m.chat);
+    var api = await fetch(`https://delirius-api-oficial.vercel.app/api/gemini?query=${encodeURIComponent(text)}`);
+    var res = await api.json();
 
-try {
-
-conn.sendPresenceUpdate('composing', m.chat)
-var apii = await fetch(`https://aemt.me/gemini?text=${text}`)
-var res = await apii.json()
-await m.reply(res.result)
-
-} catch (error) {
-console.error(error)
-return conn.reply(m.chat, `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥆ᥴᥙrrі᥆ ᥙᥒ 𝖿ᥲᥣᥣ᥆*`, m, fake, )
+    if (res.status === 'success') {
+      await conn.reply(m.chat, res.result, m);
+    } else {
+      throw new Error('API returned error');
+    }
+  } catch (error) {
+    console.error(error);
+    return conn.reply(m.chat, `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥆ᥴᥙrrі᥆ ᥙᥒ 𝖿ᥲᥣᥣ᥆*`, m);
+  }
 }
 
-}
-handler.command = ['ia', 'bard']
-handler.help = ['bard']
-handler.tags = ['ai']
+handler.command = ['ia', 'bard'];
+handler.help = ['bard'];
+handler.tags = ['ai'];
 
-handler.premium = false
+handler.premium = false;
 
-export default handler
+export default handler;
