c0c3b419d6b7f90f3b31527bbb9c435b0c3b530c
Enzo-PL-Oficial <106297033+EnzoVaselevich@users.noreply.github.com>
2024-06-22 03:07:23 -0300

    Actualizar ai-bard.js

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -1,12 +1,13 @@
 import fetch from 'node-fetch';
 
 var handler = async (m, { text, usedPrefix, command, conn }) => {
-  if (!text) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *іᥒgrᥱsᥱ ᥙᥒᥲ ⍴ᥱ𝗍іᥴі᥆́ᥒ*\n\n[ ❕ ]Ejemplo, ${usedPrefix}${command} BaileyBot-MD?`, m)
+  if (!text) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *іᥒgrᥱsᥱ ᥙᥒᥲ ⍴ᥱ𝗍іᥴі᥆́ᥒ*\n\n[ ❕ ]Ejemplo, ${usedPrefix}${command} BaileyBot-MD?`, m);
 
   try {
-    conn.sendPresenceUpdate('composing', m.chat);
+    conn.sendPresenceUpdate('composing', m);
     var api = await fetch(`https://delirius-api-oficial.vercel.app/api/gemini?query=${encodeURIComponent(text)}`);
     var res = await api.json();
+    console.log(res); 
 
     if (res.status === 'success') {
       await conn.reply(m.chat, res.result, m);
@@ -14,8 +15,11 @@ var handler = async (m, { text, usedPrefix, command, conn }) => {
       throw new Error('API returned error');
     }
   } catch (error) {
-    console.error(error);
-    return conn.reply(m.chat, `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥆ᥴᥙrrі᥆ ᥙᥒ 𝖿ᥲᥣᥣ᥆*`, m);
+    console.error('Error fetching from API:', error);
+    console.error('Response status:', api.status); 
+    
+    console.error('Response text:', await api.text()); 
+    return conn.reply(m.chat, `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 Error al procesar mensaje xd*`, m);
   }
 }
 
